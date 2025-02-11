dc7ad585b3ca05abf2936e46d080472f45939f85
Son Pham-Ba <son.phamba@epfl.ch>
2025-02-11 11:33:45 +0100

    feat: implement list remote commands

--- frame_cli/listing.py
+++ frame_cli/listing.py
@@ -1,4 +1,11 @@
 from enum import Enum
+from json import JSONDecodeError
+
+import requests
+from rich.console import Console
+from rich.table import Table
+
+from .config import API_URL
 
 
 class ComponentType(str, Enum):
@@ -6,19 +13,74 @@ class ComponentType(str, Enum):
     machine_learning = "machine-learning"
 
 
-def list_remote_hybrid_models() -> None:
+def list_remote_models() -> None:
     """List remote hybrid models."""
-    raise NotImplementedError
+    url = f"{API_URL}/hybrid_models/ids/"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print("Error fetching remote hybrid models. Check the API URL.")
+        return
+
+    try:
+        models = response.json()
+    except JSONDecodeError:
+        print("Error decoding JSON. Check the API URL.")
+        return
+
+    title = "Remote hybrid models"
+    table = Table(title=title, min_width=len(title) + 4)
+    table.add_column("Name")
+    for model in models:
+        table.add_row(model)
+
+    console = Console()
+    console.print("")
+    console.print(table)
 
 
-def list_local_hybrid_models() -> None:
+def list_local_models() -> None:
     """List installed hybrid models."""
     raise NotImplementedError
 
 
 def list_remote_components(type: ComponentType | None) -> None:
     """List remote components."""
-    raise NotImplementedError
+    url = f"{API_URL}/components/"
+    match type:
+        case None:
+            url += "ids/"
+        case ComponentType.physics_based:
+            url += "physics_based_ids/"
+        case ComponentType.machine_learning:
+            url += "machine_learning_ids/"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print("Error fetching remote components. Check the API URL.")
+        return
+
+    try:
+        models = response.json()
+    except JSONDecodeError:
+        print("Error decoding JSON. Check the API URL.")
+        return
+
+    match type:
+        case None:
+            title = "Remote components"
+        case ComponentType.physics_based:
+            title = "Remote physics-based components"
+        case ComponentType.machine_learning:
+            title = "Remote machine-learning components"
+    table = Table(title=title, min_width=len(title) + 4)
+    table.add_column("Name")
+    for model in models:
+        table.add_row(model)
+
+    console = Console()
+    console.print("")
+    console.print(table)
 
 
 def list_local_components(type: ComponentType | None) -> None:
