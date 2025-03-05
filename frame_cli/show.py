b3b1b1b46ddbd56a2f43e0e0b54eceb56c2833f9
Son Pham-Ba <son.phamba@epfl.ch>
2025-03-05 14:46:33 +0100

    feat: add show command structure

---
+++ frame_cli/show.py
@@ -0,0 +1,14 @@
+def show_remote_model(name: str) -> None:
+    print("Feature not implemented.")
+
+
+def show_local_model(name: str) -> None:
+    print("Feature not implemented.")
+
+
+def show_remote_component(name: str) -> None:
+    print("Feature not implemented.")
+
+
+def show_local_component(name: str, hybrid_model: str) -> None:
+    print("Feature not implemented.")
