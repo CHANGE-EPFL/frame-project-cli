b31c2aab6514aefdc2f68cc0af81fa333b15cc7d
Son Pham-Ba <son.phamba@epfl.ch>
2025-03-07 14:31:43 +0100

    feat: implement pull command from git

--- frame_cli/pull.py
+++ frame_cli/pull.py
@@ -1,7 +1,44 @@
-def pull_model(name: str) -> None:
+from json import JSONDecodeError
+
+import requests
+
+from .config import API_URL
+from .downloaders.git import GitDownloader
+
+
+def retrieve_model_url(name: str) -> str | None:
+    """Retrieve the URL of a hybrid model."""
+
+    url = f"{API_URL}/hybrid_models/{name}"
+    response = requests.get(url)
+
+    if response.status_code == 404:
+        print(f'Remote hybrid model "{name}" not found.')
+        return None
+
+    if response.status_code != 200:
+        print(f"Error fetching remote hybrid model ({response.status_code}). Check the API URL.")
+        return None
+
+    try:
+        info = response.json()
+    except JSONDecodeError:
+        print("Error decoding JSON. Check the API URL.")
+        return None
+
+    return info["url"]
+
+
+def pull_model(name: str, destination: str | None) -> None:
     """Download a hybrid model and seput environment."""
-    # TODO: implement
-    print("Feature not implemented.")
+    url = retrieve_model_url(name)
+    if url is None:
+        print("Error retrieving the model URL.")
+        return
+
+    # TODO: Detect whidh downloader to use
+    downloader = GitDownloader()
+    downloader.download(url, destination)
 
 
 def pull_component(name: str, model: str) -> None:
